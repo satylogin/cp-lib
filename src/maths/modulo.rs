21ada91142f7b2d3796d3bdce6d219724171a293
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2021-07-26 18:27:30 +0530

    Modular arithmetic: Changes suggested in PR review

--- src/maths/modulo.rs
+++ src/maths/modulo.rs
@@ -1,39 +1,44 @@
-type Mod = u128;
+type Data = u128;
 
-const MOD: Mod = 1e9 as Mod + 7;
+const MOD: Data = 1e9 as Data + 7;
 
 pub trait ModMath {
-    fn madd(&self, other: Mod) -> Mod;
-    fn msub(&self, other: Mod) -> Mod;
-    fn mmul(&self, other: Mod) -> Mod;
-    fn mdiv(&self, other: Mod) -> Mod;
-    fn mpow(&self, power: Mod) -> Mod;
-    fn minverse(&self) -> Mod;
+    fn madd(&mut self, other: Data) -> Data;
+    fn msub(&mut self, other: Data) -> Data;
+    fn mmul(&self, other: Data) -> Data;
+    fn mdiv(&self, other: Data) -> Data;
+    fn mpow(&self, power: Data) -> Data;
+    fn minverse(&self) -> Data;
 }
 
-impl ModMath for Mod {
-    fn madd(&self, other: Mod) -> Mod {
-        ((self % MOD) + (other % MOD)) % MOD
-    }
-    fn msub(&self, mut other: Mod) -> Mod {
-        other %= MOD;
-        if other > self % MOD {
-            ((self % MOD) + MOD - (other % MOD)) % MOD
-        } else {
-            ((self % MOD) - (other % MOD)) % MOD
+impl ModMath for Data {
+    fn madd(&mut self, other: Data) -> Data {
+        *self += other;
+        if *self >= MOD {
+            *self -= MOD;
         }
+        *self
     }
-    fn mmul(&self, other: Mod) -> Mod {
+    fn msub(&mut self, other: Data) -> Data {
+        if other > *self {
+            *self += MOD;
+        }
+        *self -= other;
+        *self
+    }
+    fn mmul(&self, other: Data) -> Data {
         ((self % MOD) * (other % MOD)) % MOD
     }
-    fn mdiv(&self, other: Mod) -> Mod {
+    fn mdiv(&self, other: Data) -> Data {
         self.mmul(other.minverse())
     }
-    fn mpow(&self, mut power: Mod) -> Mod {
-        power %= MOD - 1; // Euler's totient theorem
+
+    /// Uses Euler's totient theorem, hence only works when `self` and `MOD` are coprime
+    fn mpow(&self, mut power: Data) -> Data {
+        power %= MOD - 1;
         let mut base = *self;
         base %= MOD;
-        let mut ans: Mod = 1;
+        let mut ans: Data = 1;
         while power > 0 {
             if power & 1 != 0 {
                 ans = ans * base % MOD;
@@ -43,8 +48,9 @@ impl ModMath for Mod {
         }
         ans
     }
-    fn minverse(&self) -> Mod {
-        // Uses Fermat's Little Theorem, hence only works when MOD is prime
+
+    /// Uses Fermat's Little Theorem, hence only works when `MOD` is prime
+    fn minverse(&self) -> Data {
         self.mpow(MOD - 2)
     }
 }
@@ -54,7 +60,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(106889461, 51757006.mmul(2.mpow(293322).msub(1)));
+    fn mexp() {
+        assert_eq!(568493343, 2.mpow(293322));
     }
 }
