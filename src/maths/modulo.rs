9bf40e32592fda8d4ed06b3fc258548fe15b4751
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2021-07-26 17:30:03 +0530

    Modular arithemtic: Implement more operations, add tests

--- src/maths/modulo.rs
+++ src/maths/modulo.rs
@@ -1,25 +1,59 @@
-type Data = i64;
+type Mod = u128;
 
-const MOD: Data = 1e9 as Data + 7;
+const MOD: Mod = 1e9 as Mod + 7;
 
-pub trait Modulo {
-    fn add(&mut self, other: Data);
-
-    fn sub(&mut self, other: Data);
+pub trait ModMath {
+    fn madd(&self, other: Mod) -> Mod;
+    fn msub(&self, other: Mod) -> Mod;
+    fn mmul(&self, other: Mod) -> Mod;
+    fn mdiv(&self, other: Mod) -> Mod;
+    fn mpow(&self, power: Mod) -> Mod;
+    fn minverse(&self) -> Mod;
 }
 
-impl Modulo for Data {
-    fn add(&mut self, other: Data) {
-        *self += other;
-        if *self >= MOD {
-            *self -= MOD;
+impl ModMath for Mod {
+    fn madd(&self, other: Mod) -> Mod {
+        ((self % MOD) + (other % MOD)) % MOD
+    }
+    fn msub(&self, mut other: Mod) -> Mod {
+        other %= MOD;
+        if other > self % MOD {
+            ((self % MOD) + MOD - (other % MOD)) % MOD
+        } else {
+            ((self % MOD) - (other % MOD)) % MOD
         }
     }
-
-    fn sub(&mut self, other: Data) {
-        *self -= other;
-        if *self < 0 {
-            *self += MOD;
+    fn mmul(&self, other: Mod) -> Mod {
+        ((self % MOD) * (other % MOD)) % MOD
+    }
+    fn mdiv(&self, other: Mod) -> Mod {
+        self.mmul(other.minverse())
+    }
+    fn mpow(&self, mut power: Mod) -> Mod {
+        let mut base = *self;
+        base %= MOD;
+        let mut ans: Mod = 1;
+        while power > 0 {
+            if power & 1 != 0 {
+                ans = ans * base % MOD;
+            }
+            base = base * base % MOD;
+            power >>= 1;
         }
+        ans
+    }
+    fn minverse(&self) -> Mod {
+        // Uses Fermat's Little Theorem, hence only works when MOD is prime
+        self.mpow(MOD - 2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(106889461, 51757006.mmul(2.mpow(293322).msub(1)));
     }
 }
